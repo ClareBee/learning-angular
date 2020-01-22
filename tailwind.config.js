6156ea4710c595fb64cb26485e8f61fbd7a0028b
Clare <chblackburne@gmail.com>
2020-01-22 16:16:13 +0000

    extend theme;

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   theme: {
     backgroundColor: theme => ({
     ...theme('colors'),
-    'primary': '#3490dc',
-    'secondary': '#ffed4a',
+    'primary': '#3454D1',
+    'secondary': '#D1345B',
     'background': '#efefef',
-    })
+    'background-dark': '#AEAEAE',
+    'dark': '#0D1B1E',
+    'accent': '#34D1BF',
+  }),
+  color: theme => ({
+    ...theme('colors'),
+    'primary': '#3454D1',
+    'secondary': '#D1345B',
+    'background': '#efefef',
+    'background-dark': '#AEAEAE',
+    'dark': '#0D1B1E',
+    'accent': '#34D1BF',
+  })
   },
   variants: {},
   plugins: []
