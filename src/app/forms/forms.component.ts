fa9fa70b1d7dc3f58039985cfa49aec3578a63dc
Clare <chblackburne@gmail.com>
2020-01-22 10:22:06 +0000

    wip reactive forms syntax

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css']
 })
-export class FormsComponent implements OnInit {
+export class FormsComponent {
+  myReactiveForm = new FormGroup({
+      rating: new FormControl(''),
+      name: new FormGroup({
+        petName: new FormControl(''),
+        officialName: new FormControl(''),
+      })
+    });
 
-  constructor() { }
+  myReactiveFormBuilder = this.myFormBuilder.group({
+    rating: [''],
+    name: this.myFormBuilder.group({
+      petName: [''],
+      officialName: ['']
+    })
+  });
 
-  ngOnInit() {
-  }
+  constructor(private myFormBuilder: FormBuilder) { }
 
 }
