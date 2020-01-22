8be729259d75803eb5c8b6464cce86adc4b5ba6b
Clare <chblackburne@gmail.com>
2020-01-22 21:18:49 +0000

    update home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  underConstruction = `UnderConstruction // getting there slowly`
   constructor() { }
 
   ngOnInit() {
