f48105ec62e56e82c1aad5533c200b47b279b84b
Clare Blackburne <chblackburne@gmail.com>
2020-01-28 16:28:49 +0000

    use tailwind @apply directive for base styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,27 @@
 module.exports = {
   theme: {
     backgroundColor: theme => ({
-    ...theme('colors'),
-    'primary': '#3454D1',
-    'secondary': '#D1345B',
-    'background': '#efefef',
-    'background-dark': '#AEAEAE',
-    'dark': '#0D1B1E',
-    'accent': '#34D1BF',
-  }),
-  textColor: theme => ({
-    'primary': '#3454D1',
-    'secondary': '#D1345B',
-    'background': '#efefef',
-    'white': '#fff',
-    'background-dark': '#AEAEAE',
-    'dark': '#0D1B1E',
-    'accent': '#34D1BF',
-    'light-accent': '#90E5DC',
-  })
+      ...theme('colors'),
+      primary: '#3454D1',
+      secondary: '#D1345B',
+      background: '#efefef',
+      'background-dark': '#AEAEAE',
+      dark: '#0D1B1E',
+      accent: '#34D1BF',
+    }),
+    textColor: theme => ({
+      primary: '#3454D1',
+      secondary: '#D1345B',
+      background: '#efefef',
+      white: '#fff',
+      'background-dark': '#AEAEAE',
+      dark: '#0D1B1E',
+      accent: '#34D1BF',
+      'light-accent': '#90E5DC',
+    }),
   },
-  variants: {},
-  plugins: []
-}
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'active'],
+  },
+  plugins: [],
+};
