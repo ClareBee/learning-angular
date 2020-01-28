235e6032e1c83f0f610332362e947505848607aa
Clare Blackburne <chblackburne@gmail.com>
2020-01-28 16:56:08 +0000

    add dummy pages for accesssibility, animation and rxjs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,24 @@ import { HomeComponent } from './home/home.component';
 import { FormsComponent } from './forms/forms.component';
 import { AboutComponent } from './about/about.component';
 import { ResourcesComponent } from './resources/resources.component';
+import { AccessibilityComponent } from './accessibility/accessibility.component';
+import { AnimationsComponent } from './animations/animations.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'forms', component: FormsComponent },
   { path: 'resources', component: ResourcesComponent },
+  { path: 'animations', component: AnimationsComponent },
+  { path: 'accessibility', component: AccessibilityComponent },
+  { path: 'rxjs', component: RxjsComponent },
   { path: 'about', component: AboutComponent },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
