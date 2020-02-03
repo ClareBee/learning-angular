d0a0af5817d64e28de43fddc6aedff9f1f1fe919
Clare Blackburne <chblackburne@gmail.com>
2020-02-03 14:29:25 +0000

    notes on aria labelling

--- src/app/accessibility/accessibility.component.ts
+++ src/app/accessibility/accessibility.component.ts
@@ -9,5 +9,8 @@ export class AccessibilityComponent implements OnInit {
   constructor() {}
   ariaMarkdown = `<!-- Use attr. when binding to an ARIA attribute -->
 <button [attr.aria-label]="myActionLabel">...</button>`;
+  staticAriaMarkdown = `
+  <!-- Static ARIA attributes require no extra syntax -->
+  <button aria-label="Save document">...</button>`;
   ngOnInit() {}
 }
